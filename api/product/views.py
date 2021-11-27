f29f9b86aad4e87e504436c2004fe20cd5443f28
ashish kumar jha <61390016+ashishjha005@users.noreply.github.com>
2021-11-27 13:40:18 +0530

    Add files via upload

---
+++ api/product/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import ProductSerializer 
+from .models import Product 
+
+class ProductViewSet(viewsets.ModelViewSet):
+     queryset=Product.objects.all().order_by('id') 
+     serializer_class=ProductSerializer
